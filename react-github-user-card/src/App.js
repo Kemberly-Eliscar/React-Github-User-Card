dcdca6c8136ffefb2b1adee5294827c1439ff747
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-03 18:35:36 -0400

    completed mvp

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+ 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    }
+  }
+
+  //this is a function that will run when the component is mounted When its first created and fist put into our function.
+  componentDidMount() {
+    fetch('https://api.github.com/users/Kemberly-Eliscar')
+    .then(res => res.json())
+    .then(data => this.setState({ user: data }));
+    fetch('https://api.github.com/users/Kemberly-Eliscar/followers')
+    .then(res => res.json())
+    .then(data => this.setState({ followers: data }));
+  }
+
+  //this will only run on the re-route
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
     <div className="App">
+      <UserCard user={this.state.user} followers={this.state.followers} />
+    </div>
+    );
+  }
+}
+
+function UserCard(props) {
+  return (
+    <div>
+      <h2>{props.user.login}</h2>
+      <p>{props.user.location}</p>
+      <p>{props.user.url}</p>
+      <div>
+        {props.followers.map(followers => (
+        <div key={followers.id}>{followers.login}</div>
+        ))}
+      </div>
     </div>
   );
 }
